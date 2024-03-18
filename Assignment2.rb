eed6ef881606a753ad676fed4c693b7d868290ac
Subhajit-QI <subhajit.paul@quantuminventions.com>
2024-03-18 16:43:59 +0530

    Assignment2

---
+++ Assignment2.rb
@@ -0,0 +1,14 @@
+data = [
+  { id: 1, marks: 80 },
+  { id: 2, marks: 60 },
+  { id: 1, marks: 60 },
+  { id: 2, marks: 90 }
+]
+
+totals = {}
+
+data.group_by { |entry| entry[:id] }.each do |id, entries|
+  totals[id] = { total: entries.sum { |entry| entry[:marks] } }
+end
+
+puts totals.inspect
