e705fbdce095e46c031056801e1f06e9d765035c
Subhajit-QI <163163180+Subhajit-QI@users.noreply.github.com>
2024-03-19 08:43:31 +0530

    Update Assignment4.rb

--- Assignment4.rb
+++ Assignment4.rb
@@ -1,3 +1,5 @@
+#Write a program to check whether all element is non zero? (Method should return either true or false based on given input)
+arr = [0, 10, 0, 0, 20]
 arr = [0, 10, 0, 0, 20]
 
 result = arr.find { |num| num != 0 }
