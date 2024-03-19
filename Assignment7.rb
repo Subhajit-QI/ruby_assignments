d9f436d03d94065675358347fb920359faf6fa93
Subhajit-QI <subhajit.paul@quantuminventions.com>
2024-03-19 18:19:43 +0530

    Assignment7

--- Assignment7.rb
+++ Assignment7.rb
@@ -1,3 +1,5 @@
+#Can you write a program to find the words having bd inside it?
+# suppose you have the same set of elements in the array arr = ["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"]
 arr = ["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"]
 
 words_with_bd = []
