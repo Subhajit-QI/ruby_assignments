f2875ff49c9014f32dad2795d9a4840ccb4f6429
Subhajit-QI <163163180+Subhajit-QI@users.noreply.github.com>
2024-03-19 08:43:00 +0530

    Update Assignment3.rb

--- Assignment3.rb
+++ Assignment3.rb
@@ -1,3 +1,5 @@
+#Write a program to get first element which is non zero number.
+arr = [0, 10, 0, 0, 20]
 input_array = ['abc bcd', 'mno pqr xyz', 'efg']
 
 result_array = input_array.flat_map { |string| string.split }
