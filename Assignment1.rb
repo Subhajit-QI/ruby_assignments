98e9a161a750a595c04e05b0422b529ab447f7cc
Subhajit-QI <163163180+Subhajit-QI@users.noreply.github.com>
2024-03-19 08:41:52 +0530

    Update Assignment1.rb

--- Assignment1.rb
+++ Assignment1.rb
@@ -1,3 +1,5 @@
+#Write a program to get all the words which are starting from 'ab'.
+arr = ["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"]
 arr = ["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"]
 
 words_starting_with_ab = arr.select { |word| word.start_with?("ab") }
