e63e27f94f2c403d362fc5ec5bd1ed951cf8d51c
Subhajit-QI <subhajit.paul@quantuminventions.com>
2024-03-19 17:47:28 +0530

    106382-Assignments

--- Assignment1.rb
+++ Assignment1.rb
@@ -1,6 +1,8 @@
+
 arr = ["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"]
 
 words_starting_with_ab = arr.select { |word| word.start_with?("ab") }
 
 puts "Words starting with 'ab':"
 print words_starting_with_ab
+
