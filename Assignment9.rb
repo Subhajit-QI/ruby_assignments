8a1a5271586cecd3e7b40fe4ff2f25b8c6d9a474
Subhajit-QI <subhajit.paul@quantuminventions.com>
2024-03-21 11:15:23 +0530

    Assignment9 added

---
+++ Assignment9.rb
@@ -0,0 +1,19 @@
+#Write a class (with constructor) to define instance methods for getting area & perimeter of a rectangle.
+class Rectangle
+    def initialize(length, width)
+      @length = length
+      @width = width
+    end
+  
+    def area
+      @length * @width
+    end
+  
+    def perimeter
+      2 * (@length + @width)
+    end
+  end
+  
+  rectangle = Rectangle.new(5, 3)
+  puts "Area: #{rectangle.area}"
+  puts "Perimeter: #{rectangle.perimeter}"
