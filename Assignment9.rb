bdf2abcf94b5af742eb3028bd475981bebdb4fdd
Subhajit-QI <subhajit.paul@quantuminventions.com>
2024-03-21 12:04:13 +0530

    Assignment9 added

---
+++ Assignment9.rb
@@ -0,0 +1,18 @@
+class Rectangle
+  def initialize(length, width)
+    @length = length
+    @width = width
+  end
+  
+  def area
+    @length * @width
+  end
+  
+  def perimeter
+    2 * (@length + @width)
+  end
+end
+  
+rectangle = Rectangle.new(5, 3)
+puts "Area: #{rectangle.area}"
+puts "Perimeter: #{rectangle.perimeter}"
