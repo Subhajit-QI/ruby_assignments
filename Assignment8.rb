478e2a3cba7da910156611510cced584989da1ed
Subhajit-QI <subhajit.paul@quantuminventions.com>
2024-03-21 11:43:52 +0530

    Assigment8 added

---
+++ Assignment8.rb
@@ -0,0 +1,20 @@
+#Write a class and define a class method which will return the object count of that class.
+class MyClass
+    @@object_count = 0 
+
+    def initialize
+      @@object_count += 1 
+    end
+
+    def self.object_count
+      @@object_count 
+    end
+  end
+
+  puts MyClass.object_count 
+
+  object1 = MyClass.new
+  puts MyClass.object_count 
+
+  object2 = MyClass.new
+  puts MyClass.object_count
