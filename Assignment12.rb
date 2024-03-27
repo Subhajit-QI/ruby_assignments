80c6d3d283badf2f0ca82db862c3cbf120077edf
Subhajit-QI <subhajit.paul@quantuminventions.com>
2024-03-27 08:52:08 +0530

    Adding question title

--- Assignment12.rb
+++ Assignment12.rb
@@ -1,3 +1,7 @@
+# Write a program 
+# to define a method inside a module and then use the module in a class 
+# such a way that the method can be accessible as a class method of that 
+# class.
 module MyModule
   def my_method
     puts "This is a method defined inside the module"
@@ -9,4 +13,3 @@ class MyClass
 end
   
 MyClass.my_method
-  
